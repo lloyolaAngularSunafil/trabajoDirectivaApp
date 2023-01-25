0efb5e36f711843dcf88b2f98727e35dc2a9513b
Luis Nazario Loyola Aquino <lloyola@sunafil.gob.pe>
2023-01-25 12:45:52 -0500

    pildoras informaticas TrabajoDirectivaApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registros de Usuarios';
+  mensaje = '';
+  registrado = false;
+
+  nombre:string="";
+  apellido:string="";
+  cargo:string = "";
+  entradas: any[] ;
+
+  constructor() {
+    this.entradas = [
+        {value:"Paiton cada dia mas presentes",name:"Paiton cada dia mas presente"},
+        {value:"Java presente de hace anios",name:"dfdsf"},
+        {value:"Java Script cada vez mas funcional",name:"dfdsf "},
+        {value:"Kotlin potencia para tus apps",name:"hdfg"},
+        {value:"Donde quedo Pascal",name:" fdfds"},
+    ]
+  }
+  registrarUsuarios(): void {
+    this.mensaje="usuarios registrado con exito";
+    this.registrado=true;
+
+  }
 }
